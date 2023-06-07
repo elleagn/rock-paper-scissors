a0674a407c9a74f0a80c7d49088f8c7367b67269
elleagn <133775323+elleagn@users.noreply.github.com>
2023-06-07 07:47:16 +0000

    Add game function to manage the game
    Game keeps track of the score and announces rounds.
    
    Helper functions created :
    isValid that checks whether the player input is actually rock paper or scissors
    getPlayerSelection that manages prompting the player for a value until they enter a valid input
    
    Modification of playRound so that it console.log the messages and returnes the game result instead

--- script.js
+++ script.js
@@ -48,20 +48,65 @@ function calcGameResult (computerSelection,playerSelection) {
 
 
 function playRound (computerSelection, playerSelection) {
-    const capitalePlayerSelection=capitalize(playerSelection);
-    const gameResult = calcGameResult(computerSelection,capitalePlayerSelection);
+    let gameResult = calcGameResult(computerSelection,playerSelection);
     let message;
     switch(gameResult) {
         case 0 :
             message = 'Draw! No one wins...';
             break;
         case 1 :
-            message = `You Win! ${capitalePlayerSelection} beats ${computerSelection}`;
+            message = `You Win! ${playerSelection} beats ${computerSelection}`;
             break;
         case 2 :
-            message = `You Lose! ${computerSelection} beats ${capitalePlayerSelection}`;
+            message = `You Lose! ${computerSelection} beats ${playerSelection}`;
     }
-    return message
+    console.log(message);
+    return gameResult
 
+}
+
+function isValid (playerSelection) {
+    selectionValid=['Rock','Paper','Scissors'].includes(playerSelection);
+    return selectionValid
+}
+
+function getPlayerChoice() {
+        let playerSelection=capitalize(prompt('Rock, Paper or Scissors ?'));
+        let selectionValid=isValid(playerSelection);
+    while (!selectionValid) {
+        console.log('Something\'s wrong with your choice. Try again.');
+        playerSelection=capitalize(window.prompt('Rock, Paper or Scissors ?'));
+        selectionValid=isValid(playerSelection);
+    }
+    return playerSelection
+}
+
+function game () {
+    console.log('Welcome to rock-paper-scissors!');
+    const player = window.prompt('Please enter a username.','player');
+    let playerScore=0;
+    let computerScore=0;
+    
+    for (let roundNumber = 0;roundNumber<5;) {
+        roundNumber ++;
+        console.log(`Round ${roundNumber} : ${player} ${playerScore} - Computer ${computerScore}`);
+        let playerSelection=getPlayerChoice();
+        let computerSelection=getComputerChoice();
+        let roundResult=playRound(computerSelection,playerSelection);
+        if (roundResult==2) {
+            computerScore++;
+        } else if (roundResult==1) {
+            playerScore++;
+        }
+    }
+
+    console.log(`Final score : ${player} ${playerScore} - Computer ${computerScore}`)
+    if (playerScore>computerScore) {
+        console.log(`Congratulations ${player}, you won!`);
+    } else if (playerScore==computerScore) {
+        console.log(`Draw! No one wins...`);
+    } else {
+        console.log(`Too bad ${player} : you lose!`);
+    }
 }
 
