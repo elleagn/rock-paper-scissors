5c55ba17b525a31857b880b216e0e43ed7fe2dd5
elleagn <133775323+elleagn@users.noreply.github.com>
2023-06-13 19:22:39 +0200

    Add the GUI
    Change the condition for the game ending from 5 rounds to until
    someone's score reaches 5.
    
    Add buttons to communicate the choice.
    
    Add board to display infos for the user.

--- script.js
+++ script.js
@@ -1,113 +1,166 @@
-function getRandomInteger  (max) { 
-    return Math.floor(max*Math.random())
-
+function getRandomInteger(max) {
+  return Math.floor(max * Math.random());
 }
 
-function getRandomElement (array) { 
-    return array[getRandomInteger (array.length)]
+function getRandomElement(array) {
+  return array[getRandomInteger(array.length)];
 }
 
-
-
-function getComputerChoice () {
-    return getRandomElement(['Rock','Paper','Scissors'])
+function getComputerChoice() {
+  return getRandomElement(["Rock", "Paper", "Scissors"]);
 }
 
-function modulo (integer,modulus) {
-    const integerClass= (integer%modulus+modulus)%modulus;
-    return integerClass
+function modulo(integer, modulus) {
+  const integerClass = ((integer % modulus) + modulus) % modulus;
+  return integerClass;
 }
 
 function toNumber(selection) {
-    let numberSelection;
-    switch(selection) {
-        case 'Rock':
-            numberSelection=0;
-            break;
-        case 'Paper':
-            numberSelection=1;
-            break;
-        case 'Scissors':
-            numberSelection=2;
-    }
-    return numberSelection
+  let numberSelection;
+  switch (selection) {
+    case "Rock":
+      numberSelection = 0;
+      break;
+    case "Paper":
+      numberSelection = 1;
+      break;
+    case "Scissors":
+      numberSelection = 2;
+  }
+  return numberSelection;
 }
 
-function capitalize (string) {
-    return string.charAt(0).toUpperCase()+string.slice(1).toLowerCase()
+function calcGameResult(computerSelection, playerSelection) {
+  /*the number indicates the relative position of computerSelection regarding playerSelection*/
+  const playerNumber = toNumber(playerSelection);
+  const computerNumber = toNumber(computerSelection);
+  const gameResult = modulo(playerNumber - computerNumber, 3);
+  return gameResult;
 }
 
-
-function calcGameResult (computerSelection,playerSelection) {
-    /*the number indicates the relative position of computerSelection regarding playerSelection*/
-    const playerNumber = toNumber(playerSelection);
-    const computerNumber=toNumber(computerSelection);
-    const gameResult = modulo(playerNumber-computerNumber,3);
-    return gameResult;
+function convertTheId(id) {
+  switch (id) {
+    case "player-rock":
+      return "Rock";
+      break;
+    case "player-paper":
+      return "Paper";
+      break;
+    case "player-scissors":
+      return "Scissors";
+  }
 }
 
-
-function playRound (computerSelection, playerSelection) {
-    /*The gameResult is calculated as a number to reduce the number of cases to 3 (vs 9) */
-    let gameResult = calcGameResult(computerSelection,playerSelection);
-    let message;
-    switch(gameResult) {
-        case 0 :
-            message = 'Draw! No one wins...';
-            break;
-        case 1 :
-            message = `You Win! ${playerSelection} beats ${computerSelection}`;
-            break;
-        case 2 :
-            message = `You Lose! ${computerSelection} beats ${playerSelection}`;
-    }
-    console.log(message);
-    return gameResult
-
+function endTheGame() {
+  const message = document.getElementById("text");
+  const computerScore = Number(
+    document.getElementById("computer-score").textContent
+  );
+  const board = document.querySelector(".board");
+  board.removeChild(document.querySelector(".score"));
+  if (computerScore === 5) {
+    message.textContent = "Too bad ! You lost...";
+  } else {
+    message.textContent = "Congratulations ! You win !";
+  }
 }
 
-function isValid (playerSelection) {
-    selectionValid=['Rock','Paper','Scissors'].includes(playerSelection);
-    return selectionValid
+function playRound(event) {
+  /*The gameResult is calculated as a number to reduce the number of cases to 3 (vs 9) */
+  const buttons = document.querySelectorAll(".choice");
+  buttons.forEach((button) => {
+    button.removeEventListener("click", playRound);
+  });
+
+  const computerSelection = getComputerChoice();
+  const playerSelection = convertTheId(event.currentTarget.id);
+  let gameResult = calcGameResult(computerSelection, playerSelection);
+
+  const playerSpan = document.getElementById("player-score");
+  let playerScore = Number(playerSpan.textContent);
+  const computerSpan = document.getElementById("computer-score");
+  let computerScore = Number(computerSpan.textContent);
+  const roundSpan = document.getElementById("round-number");
+
+  let message;
+  switch (gameResult) {
+    case 0:
+      message = "Draw! No one wins...";
+      break;
+    case 1:
+      message = `You Win! ${playerSelection} beats ${computerSelection}`;
+      playerScore++;
+      playerSpan.textContent = `${playerScore}`;
+      break;
+    case 2:
+      message = `You Lose! ${computerSelection} beats ${playerSelection}`;
+      computerScore++;
+      computerSpan.textContent = `${computerScore}`;
+  }
+
+  const messageBox = document.getElementById("text");
+  messageBox.textContent = message;
+
+  if (computerScore < 5 && playerScore < 5) {
+    setTimeout(() => {
+      roundSpan.textContent = `${Number(roundSpan.textContent) + 1}`;
+      messageBox.textContent = "Chose a option to play next round.";
+      buttons.forEach((button) => {
+        button.addEventListener("click", playRound);
+      });
+    }, 1000);
+  } else {
+    setTimeout(endTheGame, 1000);
+  }
 }
 
-function getPlayerChoice() {
-        let playerSelection=capitalize(prompt('Rock, Paper or Scissors ?'));
-        let selectionValid=isValid(playerSelection);
-    while (!selectionValid) {
-        console.log('Something\'s wrong with your choice. Try again.');
-        playerSelection=capitalize(window.prompt('Rock, Paper or Scissors ?'));
-        selectionValid=isValid(playerSelection);
-    }
-    return playerSelection
+function makeButtonsClicky(buttons) {
+  buttons.forEach(function (button) {
+    button.addEventListener(
+      "mouseover",
+      () => (button.style.backgroundColor = "white")
+    );
+    button.addEventListener(
+      "mouseout",
+      () => (button.style.backgroundColor = "whitesmoke")
+    );
+  });
 }
 
-function game () {
-    console.log('Welcome to rock-paper-scissors!');
-    const player = window.prompt('Please enter a username.','player');
-    let playerScore=0;
-    let computerScore=0;
-    
-    for (let roundNumber = 0;roundNumber<5;) {
-        roundNumber ++;
-        console.log(`Round ${roundNumber} : ${player} ${playerScore} - Computer ${computerScore}`);
-        let playerSelection=getPlayerChoice();
-        let computerSelection=getComputerChoice();
-        let roundResult=playRound(computerSelection,playerSelection);
-        if (roundResult==2) {
-            computerScore++;
-        } else if (roundResult==1) {
-            playerScore++;
-        }
-    }
-
-    console.log(`Final score : ${player} ${playerScore} - Computer ${computerScore}`)
-    if (playerScore>computerScore) {
-        console.log(`Congratulations ${player}, you won!`);
-    } else if (playerScore==computerScore) {
-        console.log(`Draw! No one wins...`);
-    } else {
-        console.log(`Too bad ${player} : you lose!`);
-    }
+function initializeTheBoard() {
+  const message = document.querySelector("#text");
+  const buttons = document.querySelectorAll(".choicecontainer");
+  message.textContent = "Chose an option to play.";
+  makeButtonsClicky(buttons);
+  const paragraphs = document.querySelectorAll(".hidden");
+  paragraphs.forEach((paragraph) => {
+    paragraph.classList.remove("hidden");
+  });
+  document.getElementById("player-score").textContent = "0";
+  document.getElementById("computer-score").textContent = "0";
+  document.getElementById("round-number").textContent = "1";
+  buttons.forEach((button) => {
+    button.addEventListener("click", playRound);
+  });
 }
 
+function monitorPage() {
+  const welcome = document.querySelector("#text");
+  welcome.textContent = "Welcome to rock-paper-scissors !";
+  welcome.style.fontFamily = "grutch";
+  welcome.style.fontSize = "55px";
+  welcome.style.color = "whitesmoke";
+  const start = document.getElementById("start");
+  start.addEventListener("mouseover", () => {
+    start.style.backgroundColor = "limegreen";
+  });
+  start.addEventListener("mouseout", () => {
+    start.style.backgroundColor = "lime";
+  });
+  start.addEventListener("click", initializeTheBoard, { once: true });
+  document.querySelectorAll(".choice").forEach((button) => {
+    button.addEventListener("click", playRound);
+  });
+}
+
+monitorPage();
